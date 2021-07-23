8c985edbba706af3cb370fcc4e5a701c0787c7e5
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-07-23 05:39:51 +0100

    ggo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const purge = process.env.NODE_ENV === 'production';
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: purge,
+    content: ['./build/**/*.html']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      minHeight:{
+        '1/20': '10vh',
+      }
+    },
   },
   variants: {
     extend: {},
