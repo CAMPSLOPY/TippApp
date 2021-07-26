703a31e80a7805f0e29e25badf0e2e0d69b770bf
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-07-26 22:40:54 +0100

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    textColor: {
+      "main" : "hsl(183, 100%, 15%)",
+      "white-main": "hsl(0, 0%, 100%)"
+    },
+    placeholderColor : {
+      "darkgrayiscyan": "hsl(184, 14%, 56%)"
+    },
+    extend: {
+      gridTemplateColumns: {
+        "full" : "repeat(3, minmax(120px, 1fr))"
+      },
+      backgroundColor :{
+        'main': "hsl(185, 41%, 84%)",
+        "verydarkcyan": "hsl(183, 100%, 15%)",
+        "verylightgrayishcyan": "hsl(189, 41%, 97%)"
+      },
+      fontFamily : {
+        'google-1': "'Space Mono', monospace"
+      },
+      letterSpacing: {
+        "biggest": "1rem"
+      },
+      width: {
+        "80": "80%"
+      }
+     
+    },
   },
   variants: {
     extend: {},
