d3ff891a901b800a427cbd0a94636b5c7ca413d3
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-09-02 16:47:40 +0100

    app.js

--- public/app.js
+++ public/app.js
@@ -2,19 +2,9 @@ const billInput =  document.querySelector('.bill');
 const numberOfPeople =  document.querySelector('.number-people-input');
 const selectTipBtn =  document.querySelectorAll('.select-tip-buttons');
 const tipAmountEl =  document.querySelector('.tip-amount');
-const TotalEl =  document.querySelector('.total-amount');
+const totalEl =  document.querySelector('.total-amount');
 const ResetBtn =  document.querySelector('.resetBtn');
 const error= document.querySelector('.error-message');
-
-
-// // TO RESET THE ENTIRE APP AFTER CALCULATING
-// ResetBtn.addEventListener('click',reset);
-// function reset(){
-//     TotalEl.innerHTML = `$0.00`;
-//     tipAmountEl.innerHTML = `$0.00`;
-//     billInputEl.value = null;
-//     numberOfPeople.value = null;
-// }
 
 
 selectTipBtn.forEach(selectTip => {
@@ -24,5 +14,15 @@ selectTip.addEventListener('click', (e)=>{
     const billInputvalue = Number(billInput.value);
     const  numberOfPeopleValue = Number(numberOfPeople.value);
     const total = billInputvalue / numberOfPeopleValue;
+    const tipAmount = total * (tip / 100);
+
+    if(billInputvalue && numberOfPeopleValue){
+    totalEl.textContent = `$${Math.floor(total - tipAmount)}`;
+    tipAmountEl.textContent = `$${tipAmount.toFixed(2)}`;
+    } else if (!numberOfPeopleValue){
+        error.classList.add('show-error');
+        numberOfPeople.style.border ='1px solid red';
+        numberOfPeople.style.color = 'red'
+    }  
 })
 })
