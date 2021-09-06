29fd258ab1bc4f7037479b83df74cce9a08d8c93
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-09-06 08:44:56 +0100

    app.js

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const numberOfPeople =  document.querySelector('.number-people-input');
 const selectTipBtn =  document.querySelectorAll('.select-tip-buttons');
 const tipAmountEl =  document.querySelector('.tip-amount');
 const totalEl =  document.querySelector('.total-amount');
-const ResetBtn =  document.querySelector('.resetBtn');
+const resetBtn =  document.querySelector('.resetBtn');
 const error= document.querySelector('.error-message');
 
 
@@ -19,10 +19,22 @@ selectTip.addEventListener('click', (e)=>{
     if(billInputvalue && numberOfPeopleValue){
     totalEl.textContent = `$${Math.floor(total - tipAmount)}`;
     tipAmountEl.textContent = `$${tipAmount.toFixed(2)}`;
+    error.classList.remove('show-error');
+    numberOfPeople.style.color = 'green'
+    numberOfPeople.style.border ='1px solid hsl(172, 57%, 50%)';
     } else if (!numberOfPeopleValue){
         error.classList.add('show-error');
         numberOfPeople.style.border ='1px solid red';
         numberOfPeople.style.color = 'red'
     }  
 })
+})
+
+// clear the whole page 
+resetBtn.addEventListener('click', (e)=>{
+billInput.value =null;
+numberOfPeople.value = null;
+totalEl.innerHTML = `<div>$0.00</div>`;
+tipAmountEl.innerHTML = `<div>$0.00</div>`;
+
 })
