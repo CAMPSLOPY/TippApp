58e1737bafe8d7986008b4626d82029183a68ac6
CAMPSLOPY <54154401+CAMPSLOPY@users.noreply.github.com>
2021-09-09 02:55:47 +0100

    app.js

--- public/app.js
+++ public/app.js
@@ -5,6 +5,7 @@ const tipAmountEl =  document.querySelector('.tip-amount');
 const totalEl =  document.querySelector('.total-amount');
 const resetBtn =  document.querySelector('.resetBtn');
 const error= document.querySelector('.error-message');
+const customBtn = document.querySelector('.custom');
 
 
 selectTipBtn.forEach(selectTip => {
@@ -22,10 +23,10 @@ selectTip.addEventListener('click', (e)=>{
     error.classList.remove('show-error');
     numberOfPeople.style.color = 'green'
     numberOfPeople.style.border ='1px solid hsl(172, 57%, 50%)';
+    resetBtn.style.backgroundColor = 'hsl(172, 57%, 50%)';
     } else if (!numberOfPeopleValue){
         error.classList.add('show-error');
-        numberOfPeople.style.border ='1px solid red';
-        numberOfPeople.style.color = 'red'
+        numberOfPeople.style.border ='2px solid red';   
     }  
 })
 })
@@ -36,5 +37,12 @@ billInput.value =null;
 numberOfPeople.value = null;
 totalEl.innerHTML = `<div>$0.00</div>`;
 tipAmountEl.innerHTML = `<div>$0.00</div>`;
+resetBtn.style.backgroundColor ="#01545ae0";
+customBtn.value = "Custom"
+})
 
+customBtn.addEventListener('submit', (e) =>{
+    e.preventDefault();
+const custom = e.target.value;
+console.log(custom)
 })
